61dcbc39afaec3862f2f0a5bde85b811f00a33a5
chenkejun <chen.kejun@szrttx.com>
2018-07-14 17:18:09 +0800

    添加进注册中心

--- dev-web/src/main/java/com/dev/web/Application.java
+++ dev-web/src/main/java/com/dev/web/Application.java
@@ -3,6 +3,7 @@ package com.dev.web;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -10,6 +11,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @MapperScan(value = "com.dev.web.mapper")
 @ComponentScan(value = {"com.frame.common","com.dev.web"})
 @EnableSwagger2
+@EnableEurekaClient
 public class Application {
 
 	public static void main(String[] args) {
