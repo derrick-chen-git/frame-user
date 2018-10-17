3b2c6f6ed1c4351f505e1859b8571448dc59e8a1
derrick.chen <derrick.chen@wolaidai.com>
2018-10-17 18:11:38 +0800

    rabbitmq cofirm机制

--- dev-web/src/main/java/com/dev/web/rabbitmq/consume/Consumer.java
+++ dev-web/src/main/java/com/dev/web/rabbitmq/consume/Consumer.java
@@ -1,27 +1,33 @@
 package com.dev.web.rabbitmq.consume;
 
-import com.dev.web.entity.User;
-import com.frame.common.utils.JacksonUtils;
+import com.rabbitmq.client.Channel;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+
 @Component
+@Slf4j
 public class Consumer {
+
     @RabbitListener(queues="topic_test_queue1")
-    public void processT1(String msg) {
-        try {
-            System.out.println("topic1:"+ JacksonUtils.json2pojo(msg, User.class));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public void process(Message message, Channel channel) throws IOException {
+
+        //成功处理
+          /* channel.basicAck(message.getMessageProperties().getDeliveryTag(), true);
+                       log.info("receive: " + new String(message.getBody()));*/
+     /*  channel.basicNack();
+                    }*/
     }
+
+
     @RabbitListener(queues="topic_test_queue2")
-    public void processT2(String msg) {
-        try {
-            System.out.println("topic2:"+ JacksonUtils.json2pojo(msg, User.class));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public void processT2(Message message, Channel channel) throws IOException {
+            channel.basicAck(message.getMessageProperties().getDeliveryTag(), true);
+            log.info("receive: " + new String(message.getBody()));
+
     }
     @RabbitListener(queues="fanout_test_queue1")
     public void processF1(String msg) {
--- dev-web/src/test/java/com/dev/web/DemoApplicationTests.java
+++ dev-web/src/test/java/com/dev/web/DemoApplicationTests.java
@@ -1,7 +1,6 @@
 package com.dev.web;
 
 import com.dev.web.entity.User;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.frame.common.rabbitmq.RabbitMqUtils;
 import com.frame.common.utils.JacksonUtils;
 import org.junit.Test;
@@ -9,8 +8,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import javax.annotation.Resource;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -27,14 +24,15 @@ public class DemoApplicationTests {
 		user.setId(1L);
 		user.setName("derrick");
 		try {
-			rabbitMqUtils.convertAndSend("topic_test_exchange","key2", JacksonUtils.obj2json(user));
+			//topic1MessageSender.send(user);
+			rabbitMqUtils.send("topic_test_exchange","key1", JacksonUtils.obj2json(user));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 	@Test
 	public void sendFMsg(){
-		rabbitMqUtils.convertAndSend("fanout_test_exchange",null,"fanout消息+++++++++++++++++++");
+//		rabbitMqUtils.convertAndSend("fanout_test_exchange",null,"fanout消息+++++++++++++++++++");
 	}
 	/*@Test
 	public void sendFMsgadasda(){
